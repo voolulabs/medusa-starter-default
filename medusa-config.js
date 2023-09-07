366e0d95fb7fc440f23ce8febed534d072a94e07
Thiago Francisco Dias <44721005+ChycoFWD@users.noreply.github.com>
2023-09-06 21:58:07 -0300

    Initialize feat/admin-plugin

---
+++ medusa-config.js
@@ -0,0 +1,56 @@
+const dotenv = require("dotenv");
+
+let ENV_FILE_NAME = "";
+switch (process.env.NODE_ENV) {
+  case "production":
+    ENV_FILE_NAME = ".env.production";
+    break;
+  case "staging":
+    ENV_FILE_NAME = ".env.staging";
+    break;
+  case "test":
+    ENV_FILE_NAME = ".env.test";
+    break;
+  case "development":
+  default:
+    ENV_FILE_NAME = ".env";
+    break;
+}
+
+try {
+  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+} catch (e) {}
+
+// CORS when consuming Medusa from admin
+const ADMIN_CORS =
+  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+
+// CORS to avoid issues when consuming Medusa from a client
+const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+
+// This is the place to include plugins. See API documentation for a thorough guide on plugins.
+const plugins = [
+  `medusa-fulfillment-manual`,
+  `medusa-payment-manual`,
+  {
+    resolve: "@medusajs/admin",
+    /** @type {import('@medusajs/admin').PluginOptions} */
+    options: {
+      autoRebuild: true
+    },
+  },
+];
+/** @type {import('@medusajs/medusa').ConfigModule} */
+module.exports = {
+  projectConfig: {
+    // redis_url: REDIS_URL,
+    // For more production-like environment install PostgresQL
+    // database_url: DATABASE_URL,
+    // database_type: "postgres",
+    database_database: "./medusa-db.sql",
+    database_type: "sqlite",
+    store_cors: STORE_CORS,
+    admin_cors: ADMIN_CORS,
+  },
+  plugins,
+};
